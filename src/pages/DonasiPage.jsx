0aca816284d70d1e18335396e6ec58787801c947
syifaad <syifaadawiyah97@gmail.com>
2024-05-24 20:53:45 +0700

    nambahin isi page donasi

--- src/pages/DonasiPage.jsx
+++ src/pages/DonasiPage.jsx
@@ -4,11 +4,34 @@ const DonasiPage = () => {
   return (
     <div className="donasi">
       <Container className="w-50 pt-5">
+        <h2>Masukkan Nominal Donasi</h2>
         <Row>
-          <h2>Masukkan Nominal Donasi</h2>
-          <Col>
-            <Button>lima puluh rebu</Button>
+          <Button className="btn">
+            <img src="src/assets/smile1.png" className="me-3" />
+            lima puluh rebu
+          </Button>
+
+          <Button className="btn">
+            <img src="src/assets/smile2.png" className="me-3" />
+            lima puluh rebu
+          </Button>
+
+          <Button className="btn">
+            <img src="src/assets/smile3.png" className="me-3" />
+            lima puluh rebu
+          </Button>
+
+          <Button className="btn">
+            <img src="src/assets/smile4.png" className="me-3" />
+            lima puluh rebu
+          </Button>
+        </Row>
+        <Row>
+          <h5>Nominal lainnya</h5>
+          <Col className="col-lg-2 bg-success">
+            <h2>Rp</h2>
           </Col>
+          <Col></Col>
         </Row>
       </Container>
     </div>
